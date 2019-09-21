4f06cad13e120ce44d965e56f52de08e52c404ab
samtcifihi <la.lojban@gmail.com>
2019-09-20 20:54:06 -0400

    Randomness Implemented

--- main.go
+++ main.go
@@ -2,8 +2,32 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"time"
 )
 
 func main() {
-	fmt.Println("Execution Completed") // Debugging
+	fmt.Println("Genned Word: ", genWord()) // Debugging
+	fmt.Println("Execution Completed")      // Debugging
+}
+
+// Generate a pseudo-word for the English language
+func genWord() string {
+	onset := []string{"1", "2"}
+	nucleus := []string{"3", "4"}
+	coda := []string{"5", "6"}
+
+	word := genCluster(onset) + genCluster(nucleus) + genCluster(coda)
+
+	return word
+}
+
+// Choose a random element from an array
+func genCluster(validStrings []string) string {
+	// Seeded Random Number Generator
+	randGen := rand.New(rand.NewSource(time.Now().UnixNano()))
+
+	cluster := validStrings[randGen.Intn(len(validStrings))]
+
+	return cluster
 }
